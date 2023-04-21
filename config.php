34b875af0dd224d5e467b76dcba93d0d946c6285
AntAntonov <116178693+AntAntonov@users.noreply.github.com>
2023-04-21 10:21:04 +0200

    Update config.php

--- config.php
+++ config.php
@@ -40,6 +40,8 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+$weather_api_key = $secrets['weather_api_key'];
+
 // Set the session timeout to 30 minutes (1800 seconds)
 ini_set('session.gc_maxlifetime', 1800);
 session_set_cookie_params(1800);
